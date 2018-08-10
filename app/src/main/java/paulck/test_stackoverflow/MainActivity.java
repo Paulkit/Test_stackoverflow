e6d242fb4a8f446776029c502f845bf15a925c5b
Paul Chu <paulkit41@gmail.com>
2018-08-11 07:38:05 +0800

    How to automatically fill the fields of a form in a webview?
    https://stackoverflow.com/questions/51693296/how-to-automatically-fill-the-fields-of-a-form-in-a-webview/51700384

---
+++ app/src/main/java/paulck/test_stackoverflow/MainActivity.java
@@ -0,0 +1,46 @@
+package paulck.test_stackoverflow;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    MyWebView mWebView;
+
+
+
+    final String paypal_url = "https://www.paypal.com/welcome/signup/#/email_password";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        fillField();
+
+    }
+
+    private void fillField() {
+        mWebView = (MyWebView) findViewById(R.id.web_view);
+        mWebView.getSettings().setDomStorageEnabled(true);
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.loadUrl(paypal_url);
+
+        mWebView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                super.onPageFinished(view, url);
+
+              view.loadUrl( "javascript:window.onload= (function(){ document.getElementById('paypalAccountData_firstName').value = 'test';})();");
+
+            }
+
+        });
+
+    }
+}
+
